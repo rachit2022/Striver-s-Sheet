6508558a04d44ffa4110c55bf3c85b92042ec665
Rachit Gupta <rachitrajgupta2020@gmail.com>
2025-04-22 12:13:49 +0530

    Striver's Sheet Day 5

---
+++ src/com/striver/java/sorting/MergeSort.java
@@ -0,0 +1,51 @@
+package com.striver.java.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int i = low;
+        int j = mid + 1;
+        int k = 0;
+        while (i <= mid && j <= high) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            } else {
+                temp[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        while (i <= mid) {
+            temp[k] = arr[i];
+            i++;
+            k++;
+        }
+        while (j <= high) {
+            temp[k] = arr[j];
+            j++;
+            k++;
+        }
+        System.arraycopy(temp, 0, arr, low, temp.length);
+
+        System.out.println("After merging from " + low + " to " + high + ": " + Arrays.toString(temp));
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 2, 1, 6, 7 };
+        mergeSort(arr, 0, arr.length - 1);
+    }
+}
